815e6ccae05040f1136bb8f2801db88657453b75
Niko Sardas <nko.srds@gmail.com>
2025-04-15 17:53:35 -0400

    Updates

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  assetPrefix: "/niko-portfolio",
-  basePath: "/nikosardas.github.io",
-  output: "export",
-};
-
-export default nextConfig;
